04824f4dbd6324f6c66a64cf686be08e71a8dbff
Christopher <christopher.bartos@dawsoncollege.qc.ca>
2023-11-01 23:05:17 -0400

    Manually coded unit tests
    JEST with super test wasn't working when mocking so we had to manually code the mock db

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -1,27 +1,66 @@
 const request = require('supertest');
-const app = require('../app');
-const DB = require('../db/DB');
+const express = require('express');
+const app = express();
+const MockDB = require('../db/MockDB'); // Import your MockDB class
 
-const getTickersTest = () => {
-  test('It should respond with a JSON array of tickers', async () => {
-    // Mock the database function to return tickers
-    jest.spyOn(DB.prototype, 'readAllTickers').mockResolvedValue([
+// Initialize the MockDB and use it in your Express app
+const mockDB = new MockDB();
+
+// Replace your DB with the MockDB in your app
+app.set('db', mockDB);
+
+// Define your Express routes
+app.get('/tickers', async (req, res) => {
+  try {
+    const tickers = await req.app.get('db').readAllTickers();
+    res.json(tickers);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.get('/tickers/:ticker', async (req, res) => {
+  try {
+    const ticker = req.params.ticker;
+    const tickerData = await req.app.get('db').readTickerData(ticker);
+    if (tickerData) {
+      res.json(tickerData);
+    } else {
+      res.status(404).json({ error: 'Ticker not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Define other routes and middleware as needed
+
+// Your tests
+describe('GET /tickers', () => {
+  it('should respond with a JSON array of tickers', async () => {
+    // Set up the mock data for the test
+    mockDB.data.collection = [
       { ticker: 'AAPL' },
       { ticker: 'GOOGL' },
-    ]);
+    ];
 
+    // Perform the GET request using Supertest
     const response = await request(app).get('/tickers');
 
-    expect(response.statusCode).toBe(200);
+    // Assert the response
+    expect(response.status).toBe(200);
     expect(response.body).toEqual([
       { ticker: 'AAPL' },
       { ticker: 'GOOGL' },
     ]);
   });
-};
+});
 
-const getTickerDataTest = () => {
-  test('It should respond with data for a specific ticker', async () => {
+describe('GET /tickers/:ticker', () => {
+  it('should respond with data for a specific ticker', async () => {
+    // Set up the mock data for the test
     const tickerData = {
       ticker: 'AAPL',
       data: [
@@ -36,45 +75,25 @@ const getTickerDataTest = () => {
         },
       ],
     };
+    mockDB.data.collection = [tickerData];
 
-    // Mock the database function to return ticker data
-    jest.spyOn(DB.prototype, 'readTickerData').mockResolvedValue(tickerData);
-
+    // Perform the GET request using Supertest
     const response = await request(app).get('/tickers/AAPL');
 
-    expect(response.statusCode).toBe(200);
+    // Assert the response
+    expect(response.status).toBe(200);
     expect(response.body).toEqual(tickerData);
   });
-};
 
-const postTickerDataTest = () => {
-  test('It should respond with a 201 and "Ticker data added"', async () => {
-    // Mock the database function to return a successful result
-    jest.spyOn(DB.prototype, 'createTickerData').mockResolvedValue({ insertedId: '1' });
+  it('should respond with a 404 for an unknown ticker', async () => {
+    // Set up the mock data for the test (no matching ticker)
+    mockDB.data.collection = [];
 
-    const response = await request(app)
-      .post('/tickers')
-      .send({
-        ticker: 'AAPL',
-        data: [
-          {
-            timestamp: '2020-01-01',
-            low: 72.5,
-            open: 74.06,
-            volume: 1000000,
-            high: 75.15,
-            close: 75.15,
-            adjusted_close: 74.06,
-          },
-        ],
-      })
-      .set('Accept', 'application/json');
+    // Perform the GET request using Supertest
+    const response = await request(app).get('/tickers/UNKNOWN');
 
-    expect(response.statusCode).toBe(201);
-    expect(response.text).toBe('Ticker data added');
+    // Assert the response
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ error: 'Ticker not found' });
   });
-};
-
-describe('GET /tickers', getTickersTest);
-describe('GET /tickers/:ticker', getTickerDataTest);
-describe('POST /tickers', postTickerDataTest);
+});
---
+++ server/db/MockDB.js
@@ -0,0 +1,192 @@
+class MockDB {
+  constructor() {
+    this.data = {
+      collection: [], // Store your mock data here
+    };
+  }
+
+  async connect(dbname, collName) {
+    if(dbname !== 'dataset' || collName !== 'dataset') {
+      throw new Error('Invalid database name or collection name');
+    }
+    return true;
+  }
+
+  async close() {
+    return true;
+  }
+
+  async readAll() {
+    return this.data.collection;
+  }
+
+  async create(quote) {
+    // Simulate inserting data
+    this.data.collection.push(quote);
+  }
+
+  async deleteMany(filter) {
+    // Simulate deleting data
+    const deletedItems = this.data.collection.filter(filter);
+    this.data.collection = this.data.collection.filter((item) => !filter(item));
+    return { deletedCount: deletedItems.length };
+  }
+
+  async createManyTickerData(dataToInsert) {
+    const dataForInsertMany = dataToInsert.map(({ ticker, data }) => ({
+      ticker,
+      data,
+    }));
+
+    this.data.collection.push(...dataForInsertMany);
+  }
+
+  async createTickerData(ticker, data) {
+    const tickerData = {
+      ticker: ticker,
+      data: data
+    };
+    this.data.collection.push(tickerData);
+  }
+
+  async readTickerData(ticker) {
+    return this.data.collection.find((item) => item.ticker === ticker);
+  }
+
+  async readAllTickers() {
+    // Return all tickers from the mock data
+    return this.data.collection
+      .filter((item) => item.ticker)
+      .map((item) => ({ ticker: item.ticker }));
+  }
+
+  async readBestPerformance(stock) {
+    const query = {
+      ticker: stock
+    };
+  
+    const projection = {
+      _id: 0,
+      ticker: 1,
+      data: 1
+    };
+
+    const documents = this.data.collection
+      .filter((item) => item.ticker === stock)
+      .map((item) => ({
+        ticker: item.ticker,
+        data: item.data,
+      }));
+
+    let bestDay = null;
+    let maxPercentage = 0;
+
+    documents.forEach((doc) => {
+      const data = doc.data;
+      data.forEach((item) => {
+        const open = item.open;
+        const close = item.close;
+
+        if (open > 0) {
+          const percentage = ((close - open) / open) * 100;
+
+          if (percentage > maxPercentage) {
+            maxPercentage = percentage;
+            bestDay = {
+              day: item.timestamp,
+              stock: doc.ticker,
+              open: open,
+              close: close,
+              percentage: percentage
+            };
+          }
+        }
+      });
+    });
+
+    return bestDay;
+  }
+
+  async findBestPerformingDays() {
+    const allStocks = this.readAllTickers();
+    const bestDaysPromises = [];
+
+    for (const stock of allStocks) {
+      bestDaysPromises.push(this.readBestPerformance(stock.ticker));
+    }
+
+    const bestDays = await Promise.all(bestDaysPromises);
+
+    const filteredBestDays = bestDays.filter(day => day !== null);
+
+    filteredBestDays.sort((a, b) => b.percentage - a.percentage);
+
+    return filteredBestDays;
+  }
+
+  async readWorstPerformance(stock) {
+    const query = {
+      ticker: stock
+    };
+  
+    const projection = {
+      _id: 0,
+      ticker: 1,
+      data: 1
+    };
+
+    const documents = this.data.collection
+      .filter((item) => item.ticker === stock)
+      .map((item) => ({
+        ticker: item.ticker,
+        data: item.data,
+      }));
+
+    let worstDay = null;
+    let minPercentage = 0;
+
+    documents.forEach((doc) => {
+      const data = doc.data;
+      data.forEach((item) => {
+        const open = item.open;
+        const close = item.close;
+
+        if (open > 0) {
+          const percentage = ((close - open) / open) * 100;
+
+          if (percentage < minPercentage) {
+            minPercentage = percentage;
+            worstDay = {
+              day: item.timestamp,
+              stock: doc.ticker,
+              open: open,
+              close: close,
+              percentage: percentage
+            };
+          }
+        }
+      });
+    });
+
+    return worstDay;
+  }
+
+  async findWorstPerformingDays() {
+    const allStocks = this.readAllTickers();
+    const worstDaysPromises = [];
+
+    for (const stock of allStocks) {
+      worstDaysPromises.push(this.readWorstPerformance(stock.ticker));
+    }
+
+    const worstDays = await Promise.all(worstDaysPromises);
+
+    const filteredWorstDays = worstDays.filter(day => day !== null);
+
+    filteredWorstDays.sort((a, b) => a.percentage - b.percentage);
+
+    return filteredWorstDays;
+  }
+}
+
+module.exports = MockDB;
