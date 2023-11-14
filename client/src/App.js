2d76d1bde76de479bc29fdaf9bd7c813854f7a10
onipicotte <onipicotte@gmail.com>
2023-11-14 15:33:25 -0500

    added regex validation

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import Chart from './Chart/Chart';
 import { useState } from 'react';
 
 function App() {
-  var tickers = ['RE', 'REGN', 'RF']
   const [selectedTickers, setSelectedTickers] = useState([])
+  
   return (
     <>
       <Search selectedTickers={selectedTickers} setSelectedTickers={setSelectedTickers} />
--- client/src/Search/SearchResult/SearchResult.jsx
+++ client/src/Search/SearchResult/SearchResult.jsx
@@ -7,16 +7,20 @@ function SearchResult({results, search, selectedTickers, setSelectedTickers}) {
       <ul className='SearchResultUl'>
 
         {results.map((result) => {
-          let ticker = result.ticker
-          let formatTicker = ticker.replace(search.toUpperCase(), '<b>' + search.toUpperCase() + '</b>')
-          return (
-            <li 
-              key={result.ticker}
-              className='SearchResultList'
-              onClick={() => setSelectedTickers(oldTickers => [...oldTickers, ticker])}>
-                {parse(formatTicker)}
-            </li>
-          );
+          const ticker = result.ticker;
+          const regex = /<|>|\//;
+          if (!ticker.match(regex)) {
+            let formatTicker = ticker.replace(search.toUpperCase(), '<b>' + search.toUpperCase() + '</b>');
+            return (
+              <li 
+                key={result.ticker}
+                className='SearchResultList'
+                onClick={() => setSelectedTickers(oldTickers => [...oldTickers, ticker])}>
+                  {parse(formatTicker)}
+              </li>
+            );
+          }
+          return null;
         })}
       </ul>
     </div>
