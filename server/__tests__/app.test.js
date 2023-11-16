c11f21153f4a7fb9030543226921212eb90fea9e
PepeLaugh <charles-alexandre.bouchard@dawsoncollege.qc.ca>
2023-11-15 19:31:37 -0500

    add tests for performance

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -13,7 +13,7 @@ describe('GET /tickers', () => {
     ]);
 
     const response = await request(app).get('/tickers');
-    //if plain text, use text, if json use body
+    // if plain text, use text, if JSON use body
     expect(response.body).toEqual([
       { ticker: 'AAPL' },
       { ticker: 'GOOGL' },
@@ -41,11 +41,92 @@ describe('GET /tickers/AAPL', () => {
     };
 
     jest.spyOn(DB.prototype, 'readTickerData').mockResolvedValue(tickerData);
-    const response = await request(app).get('/tickers/AAPL')
+    const response = await request(app).get('/tickers/AAPL');
 
-    //if plain text, use text, if json use body
+    // if plain text, use text, if JSON use body
     expect(response.body).toEqual(tickerData);
     expect(response.statusCode).toBe(200);
     expect(response.type).toEqual('application/json');
   });
 });
+
+describe('GET /high/:stock', () => {
+  test('should respond with JSON containing the best ticker for a specific stock', async () => {
+    DB.prototype.readBestPerformance.mockResolvedValue({ ticker: 'AAPL' });
+
+    const response = await request(app).get('/high/XYZ');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ ticker: 'AAPL' });
+  });
+
+  test('should handle errors and respond with 404 and error message', async () => {
+    DB.prototype.readBestPerformance.mockRejectedValue(new Error('Database error'));
+
+    const response = await request(app).get('/high/XYZ');
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({ error: 'No data found' });
+  });
+});
+
+describe('GET /highs', () => {
+  test('should respond with JSON containing the best performing tickers', async () => {
+    DB.prototype.findBestPerformingDays.mockResolvedValue([{ ticker: 'AAPL' }, { ticker: 'GOOGL' }]);
+
+    const response = await request(app).get('/highs');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([{ ticker: 'AAPL' }, { ticker: 'GOOGL' }]);
+  });
+
+  test('should handle errors and respond with 404 and error message', async () => {
+    DB.prototype.findBestPerformingDays.mockRejectedValue(new Error('Database error'));
+
+    const response = await request(app).get('/highs');
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({ error: 'No data found' });
+  });
+});
+
+describe('GET /low/:stock', () => {
+  test('should respond with JSON containing the worst ticker for a specific stock', async () => {
+    DB.prototype.readWorstPerformance.mockResolvedValue({ ticker: 'XYZ' });
+
+    const response = await request(app).get('/low/ABC');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ ticker: 'XYZ' });
+  });
+
+  test('should handle errors and respond with 404 and error message', async () => {
+    DB.prototype.readWorstPerformance.mockRejectedValue(new Error('Database error'));
+
+    const response = await request(app).get('/low/ABC');
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({ error: 'No data found' });
+  });
+});
+
+describe('GET /lows', () => {
+  test('should respond with JSON containing the worst performing tickers', async () => {
+    DB.prototype.findWorstPerformingDays.mockResolvedValue([{ ticker: 'XYZ' }, { ticker: 'LMN' }]);
+
+    const response = await request(app).get('/lows');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([{ ticker: 'XYZ' }, { ticker: 'LMN' }]);
+  });
+
+  test('should handle errors and respond with 404 and error message', async () => {
+    DB.prototype.findWorstPerformingDays.mockRejectedValue(new Error('Database error'));
+
+    const response = await request(app).get('/lows');
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({ error: 'No data found' });
+  });
+});
+
