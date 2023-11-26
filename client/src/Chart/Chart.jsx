449e71e6eee8a6f004caa320da6a959117865d23
Christopher <christopher.bartos@dawsoncollege.qc.ca>
2023-11-26 16:11:42 -0500

    Alter graphing
    Have the graph use candlesticks and improve the x axis

--- client/src/Chart/Chart.jsx
+++ client/src/Chart/Chart.jsx
@@ -1,19 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chart.css';
 import Plot from 'react-plotly.js';
-import { useEffect } from 'react';
 
-/**
- * A component that displays a chart comparing the stock prices of multiple tickers.
- * @param {Object} props - The component props.
- * @param {string[]} props.tickers - An array of ticker symbols to compare.
- * @returns {JSX.Element} - The Chart component.
- */
 function Chart({ tickers }) {
+  // State hooks
   const [tickerData, setTickerData] = useState([]);
   const [selectedTicker, setSelectedTicker] = useState(null);
   const [selectedTickerInfo, setSelectedTickerInfo] = useState(null);
 
+  // Fetch data for all tickers when component mounts
   useEffect(() => {
     const fetchData = async (ticker) => {
       const res = await fetch(`/tickers/${ticker}`);
@@ -24,21 +19,24 @@ function Chart({ tickers }) {
     const fetchAllTickers = async () => {
       const data = await Promise.all(tickers.map((ticker) => fetchData(ticker.ticker)));
       setTickerData(data);
-    };    
+    };
 
     fetchAllTickers();
   }, [tickers]);
 
+  // Handle button click for individual ticker
   const handleTickerButtonClick = (ticker) => {
     setSelectedTicker(ticker);
     updateSelectedTickerInfo(ticker);
   };
 
+  // Handle button click to show all tickers
   const handleShowAllButtonClick = () => {
     setSelectedTicker(null);
     setSelectedTickerInfo(null);
   };
 
+  // Update selected ticker information
   const updateSelectedTickerInfo = (ticker) => {
     const tickerIndex = tickers.findIndex((t) => t.ticker === ticker);
     const selectedTickerData = tickerData[tickerIndex];
@@ -53,108 +51,97 @@ function Chart({ tickers }) {
         lowestPrice,
       });
     } else {
-      // Handle the case where selectedTickerData is not found
       setSelectedTickerInfo(null);
     }
   };
 
-  if (tickerData) {
-    let plotData = [];
-  
-    if (tickerData.length > 0) {
-      const commonDates = getCommonDates(tickerData);
-  
-      // Plot data for each selected stock
-      plotData = tickers.map((ticker, index) => {
-        const adjustedCloseValues = getAdjustedCloseValues(tickerData[index]);
-  
-        if (adjustedCloseValues.length > 0) {
-          const relativePrices = adjustedCloseValues.map((value) => value / adjustedCloseValues[0]);
-  
-          return {
-            x: commonDates,
-            y: relativePrices,
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: { color: tickerData[index].color },
-            name: `Price Relative - ${ticker.ticker}`,
-            visible: selectedTicker === null || selectedTicker === ticker.ticker,
-          };
-        }
-  
-        // Handle the case where adjustedCloseValues is empty
-        return null;
-      }).filter(Boolean); // Filter out null values
-    } else {
-      // Default trace for an empty chart
-      plotData.push({
-        x: [/* provide default x values */],
-        y: [/* provide default y values */],
-        type: 'scatter',
-        mode: 'lines+markers',
-        marker: { color: '#AAA' },  // or any default color you want
-        name: 'No Data Available',
-      });
-    }
+  // Prepare candlestick chart data
+  const candlestickData = selectedTicker
+    ? tickerData
+        .filter((ticker) => ticker.ticker === selectedTicker)
+        .map((ticker) => ({
+          x: getCommonDates([ticker]),
+          close: ticker.data.map((row) => row.close),
+          high: ticker.data.map((row) => row.high),
+          low: ticker.data.map((row) => row.low),
+          open: ticker.data.map((row) => row.open),
+          increasing: { line: { color: 'black' } },
+          decreasing: { line: { color: 'red' } },
+          type: 'candlestick',
+          xaxis: 'x',
+          yaxis: 'y',
+          name: `Price Relative - ${ticker.ticker}`,
+          visible: selectedTicker === null || selectedTicker === ticker.ticker,
+        }))
+    : tickerData.map((ticker) => ({
+        x: getCommonDates(tickerData),
+        close: ticker.data.map((row) => row.close),
+        high: ticker.data.map((row) => row.high),
+        low: ticker.data.map((row) => row.low),
+        open: ticker.data.map((row) => row.open),
+        increasing: { line: { color: 'black' } },
+        decreasing: { line: { color: 'red' } },
+        type: 'candlestick',
+        xaxis: 'x',
+        yaxis: 'y',
+        name: `Price Relative - ${ticker.ticker}`,
+        visible: selectedTicker === null || selectedTicker === ticker.ticker,
+      }));
 
-    const layout = {
-      autosize: true,
-      width: Math.round(window.innerWidth * 1),
-      height: Math.round(window.innerHeight * 0.9),
-      title: `Stock Prices Comparison - ${selectedTicker || 'All Stocks'}`,
-      updatemenus: [
-        {
-          x: 0.1,
-          y: 1.15,
-          xref: 'paper',
-          yref: 'paper',
-          showactive: false,
-          buttons: [
-            {
-              method: 'relayout',
-              args: ['showlegend', true],
-              label: 'Show Legend',
-            },
-            {
-              method: 'relayout',
-              args: ['showlegend', false],
-              label: 'Hide Legend',
-            },
-          ],
-        },
-      ],
-    };
+  // Layout for the candlestick chart
+  const layout = {
+    autosize: true,
+    dragmode: 'zoom',
+    width: Math.round(window.innerWidth * 1),
+    height: Math.round(window.innerHeight * 0.9),
+    title: `Stock Prices Comparison - ${selectedTicker || tickerData ? 'All Stocks' : 'No Stocks Added'}`,
+    showlegend: false,
+    xaxis: {
+      autorange: true,
+      title: 'Date',
+      rangeselector: {
+        x: 0,
+        y: 1.2,
+        xanchor: 'left',
+        font: { size: 8 },
+        buttons: [
+          { step: 'month', stepmode: 'backward', count: 1, label: '1 month' },
+          { step: 'month', stepmode: 'backward', count: 6, label: '6 months' },
+          { step: 'all', label: 'All dates' },
+        ],
+      },
+    },
+    yaxis: {
+      autorange: true,
+    },
+  };
 
-    return (
-      <div className="chart">
-        <div className="button-container">
-          <button onClick={handleShowAllButtonClick} className={selectedTicker === null ? 'active' : ''}>
-            Show All
+  return (
+    <div className="chart">
+      <div className="button-container">
+        <button onClick={handleShowAllButtonClick} className={selectedTicker === null ? 'active' : ''}>
+          Show All
+        </button>
+        {tickers.map((ticker) => (
+          <button
+            key={ticker.ticker}
+            onClick={() => handleTickerButtonClick(ticker.ticker)}
+            className={selectedTicker === ticker.ticker ? 'active' : ''}
+          >
+            {ticker.ticker}
           </button>
-          {tickers.map((ticker) => (
-            <button
-              key={ticker.ticker}
-              onClick={() => handleTickerButtonClick(ticker.ticker)}
-              className={selectedTicker === ticker.ticker ? 'active' : ''}
-            >
-              {ticker.ticker}
-            </button>
-          ))}
-        </div>
-        <Plot data={plotData} layout={layout} />
-        {selectedTickerInfo && (
-          <div className="selected-ticker-info">
-            <p>Highest Price: ${selectedTickerInfo.highestPrice.toFixed(2)}</p>
-            <p>Lowest Price: ${selectedTickerInfo.lowestPrice.toFixed(2)}</p>
-          </div>
-        )}
+        ))}
       </div>
-    );
-  }
-
-  return null;
+      <Plot data={candlestickData} layout={layout} />
+      {selectedTickerInfo && (
+        <div className="selected-ticker-info">
+          <p>Highest Price: ${selectedTickerInfo.highestPrice.toFixed(2)}</p>
+          <p>Lowest Price: ${selectedTickerInfo.lowestPrice.toFixed(2)}</p>
+        </div>
+      )}
+    </div>
+  );
 }
-
 
 /**
  * Returns an array of common dates from an array of ticker data.
