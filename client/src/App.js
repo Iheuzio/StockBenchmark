9d06d2da51c221f6d4513007ab41b557dc10c525
onipicotte <onipicotte@gmail.com>
2023-11-28 14:06:20 -0500

    added jsdoc, comment, and cleaned code

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Chart from './Chart/Chart';
 import { useState } from 'react';
 
 function App() {
+  // Init State
   const [selectedTickers, setSelectedTickers] = useState([{ticker:'REPYY', color:""}])
   
   return (
